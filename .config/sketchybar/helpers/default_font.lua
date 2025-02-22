dd4aa8c4be571d8de40a12c12b2ac5204e045a8f
Jason King <jk@radical.gd>
2025-02-21 19:12:47 -0600

    New sketchybar

---
+++ .config/sketchybar/helpers/default_font.lua
@@ -0,0 +1,13 @@
+return {
+	text = "JetBrainsMono Nerd Font", -- Used for text
+	numbers = "JetBrainsMono Nerd Font", -- Used for numbers
+
+  -- Unified font style map
+  style_map = {
+    ["Regular"] = "Regular",
+    ["Semibold"] = "Semibold",
+    ["Bold"] = "Bold",
+    ["Heavy"] = "Heavy",
+    ["Black"] = "Black",
+  }
+}
