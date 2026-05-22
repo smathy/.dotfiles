e3576164ca821fa19b0d0eb04fb789c805fb4d8d
Jason King <jk@radical.gd>
2026-05-22 16:19:24 -0500

    lspconfig -> vim.lsp.config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -136,9 +136,6 @@ require('lazy').setup({
     },
   },
 
-  {
-  },
-
   -- Useful plugin to show you pending keybinds.
   {
     'folke/which-key.nvim',
@@ -652,103 +649,65 @@ vim.keymap.set({ "n", "v" }, "<c-s>",     "<Cmd>Treewalker Right<CR>", { silent 
 -- }}}
 
 -- {{{ LSP
-local on_attach = function(_, bufnr)
-  local nmap = function(keys, func, desc)
-    if desc then
-      desc = 'LSP: ' .. desc
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local bufnr = args.buf
+    local nmap = function(keys, func, desc)
+      vim.keymap.set('n', keys, func, { buffer = bufnr, desc = 'LSP: ' .. desc })
     end
 
-    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-  end
+    -- nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+    nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+    nmap('<leader>D',  vim.lsp.buf.type_definition, 'Type [D]efinition')
+    nmap('K',          vim.lsp.buf.hover, 'Hover Documentation')
+    nmap('<C-k>',      vim.lsp.buf.signature_help, 'Signature Documentation')
+    nmap('gD',         vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+    nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+    nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+    nmap('<leader>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, '[W]orkspace [L]ist Folders')
 
-  -- nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-  nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+    vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+      vim.lsp.buf.format()
+    end, { desc = 'Format current buffer with LSP' })
+  end,
+})
 
-  -- See `:help K` for why this keymap
-  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+vim.lsp.config('*', {
+  capabilities = vim.lsp.protocol.make_client_capabilities(),
+})
 
-  -- Lesser used LSP functionality
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  nmap('<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, '[W]orkspace [L]ist Folders')
-
-  -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-    vim.lsp.buf.format()
-  end, { desc = 'Format current buffer with LSP' })
-end
-
-local lspconfig = require'lspconfig'
-local servers = {
-  lua_ls = {
+vim.lsp.config('lua_ls', {
+  settings = {
     Lua = {
-      workspace = { checkThirdParty = false },
-      telemetry = { enable = false },
+      workspace   = { checkThirdParty = false },
+      telemetry   = { enable = false },
       diagnostics = { globals = { "vim", "Snacks" }, disable = { 'missing-fields' } },
     },
   },
+})
 
-  gopls = {},
-}
-
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-
-local mason_lspconfig = require 'mason-lspconfig'
-
-mason_lspconfig.setup {
-  ensure_installed = vim.tbl_keys(servers),
-}
-
--- mason_lspconfig.setup_handlers {
---   function(server_name)
---     lspconfig[server_name].setup {
---       capabilities = capabilities,
---       on_attach = on_attach,
---       settings = servers[server_name],
---       filetypes = (servers[server_name] or {}).filetypes,
---       init_options = (servers[server_name] or {}).init_options,
---     }
---   end
--- }
-
-local configs = require("lspconfig.configs")
-
-local lexical_config = {
-  filetypes = { "html", "elixir", "eelixir", "heex" },
-  cmd = { os.getenv('HOME') .. "/bin/lexical/bin/start_lexical.sh" },
-  settings = { dialyzerEnabled = false },
-}
-
-if not configs.lexical then
-  configs.lexical = {
-    default_config = {
-      filetypes = lexical_config.filetypes,
-      cmd = lexical_config.cmd,
-      root_dir = function(fname)
-        return lspconfig.util.root_pattern("mix.exs", ".git")(fname) or vim.loop.os_homedir()
-      end,
-      -- optional settings
-      settings = lexical_config.settings,
-      on_attach = on_attach,
-    },
-  }
-end
-
-lspconfig.lexical.setup({})
-
-lspconfig.ruby_lsp.setup {
-  on_attach = on_attach,
+vim.lsp.config('ruby_lsp', {
   init_options = {
-    indexing = {
-      excluded_patterns = { "PERF-*/**/*" },
-    },
+    indexing = { excluded_patterns = { "PERF-*/**/*" } },
   },
+})
+
+vim.lsp.config('lexical', {
+  cmd          = { vim.env.HOME .. '/bin/lexical/bin/start_lexical.sh' },
+  filetypes    = { "html", "elixir", "eelixir", "heex" },
+  root_markers = { "mix.exs", ".git" },
+  settings     = { dialyzerEnabled = false },
+})
+
+require('mason-lspconfig').setup {
+  ensure_installed = { 'lua_ls', 'gopls' },
+  automatic_enable = true,
 }
+
+vim.lsp.enable({ 'lexical', 'ruby_lsp' })
 
 vim.lsp.log.set_level("error")
 -- }}}
