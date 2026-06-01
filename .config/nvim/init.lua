9c93528708f7626f8a149331967b8987d9417c21
Jason King <jk@radical.gd>
2026-06-01 13:13:14 -0500

    Snacks update, remove avante

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -27,7 +27,7 @@ bo.autoindent = true
 bo.cindent = true
 opt.mousefocus = true
 opt.laststatus = 3
-opt.titlestring = '%(  %{substitute(expand("%:~"), "/work/", "", "")}%)'
+opt.titlestring = '%(  %{substitute(expand("%:~"), "/work/juno", "juno", "")}%)'
 
 opt.tabstop = 2
 opt.shiftwidth = 0
@@ -432,57 +432,6 @@ require('lazy').setup({
     },
   },
 
-  {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    version = false, -- Never set this value to "*"! Never!
-    opts = {
-      behaviour = {
-        enable_claude_text_editor_tool_mode = true,
-      },
-      web_search_engine = {
-        provider = "brave",
-      },
-      hints = { enabled = false },
-      file_selector = { provider = "snacks" },
-    },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "folke/snacks.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "stevearc/dressing.nvim",
-      icons_lib,
-      -- {
-      --   -- support for image pasting
-      --   "HakonHarnes/img-clip.nvim",
-      --   event = "VeryLazy",
-      --   opts = {
-      --     -- recommended settings
-      --     default = {
-      --       embed_image_as_base64 = false,
-      --       prompt_for_file_name = false,
-      --       drag_and_drop = {
-      --         insert_mode = true,
-      --       },
-      --       -- required for Windows users
-      --       use_absolute_path = true,
-      --     },
-      --   },
-      -- },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
-    },
-  },
 }, {})
 -- }}}
 
